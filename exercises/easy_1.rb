7428b176873eb44e8d5040a5096563710a4b28fb
Joe <joe@example.com>
2016-01-14 14:58:45 -0500

    easy 1

---
+++ exercises/easy_1.rb
@@ -0,0 +1,41 @@
+# Question 1
+# obj.class
+
+# Question 2
+
+# use include Speed
+# use respond_to? :go_fast on the Car or Truck instance
+
+# Question 3
+
+# self.class. self refers to the object itself. By calling #class on it it returns the class name
+
+# Question 4
+
+# AngryCat.new
+
+# Question 5
+
+# the Pizza class. since it has an @ sign
+# You also call instance_variables after instantiating an object
+
+# Question 6
+
+# create a getter method
+
+# Question 7
+
+# object’s class and an encoding of the object id
+
+# Question 8
+
+# the instance (object)
+
+# Question 9
+
+# since self isn't inside an instance method it refers to class itself
+
+# Question 10
+
+# pass in the required arguments to the Object#new method
+ 
